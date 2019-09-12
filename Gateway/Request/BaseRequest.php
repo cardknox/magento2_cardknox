0d91920d616f26cc3075b5734ef641f5eac5f84f
dehrentrei-ck <dehrentrei@cardknox.com>
2019-09-12 10:20:02 -0400

    added validation for cc and cvv for adminhtml, updated to version 1.0.9

--- Gateway/Request/BaseRequest.php
+++ Gateway/Request/BaseRequest.php
@@ -56,7 +56,7 @@ class BaseRequest implements BuilderInterface
         return [
             'xVersion' => '4.5.8',
             'xSoftwareName' => 'Magento ' . $this->productMetadata->getEdition() . " ". $this->productMetadata->getVersion(),
-            'xSoftwareVersion' => '1.0.8',
+            'xSoftwareVersion' => '1.0.9',
             'xKey' => $this->config->getValue(
                 'cardknox_transaction_key',
                 $order->getStoreId()
