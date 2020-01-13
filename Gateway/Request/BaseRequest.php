6f912e0c12815125029e3d311a2e28abb89c330f
mstein <mstein@cardknox.com>
2020-01-13 10:46:14 -0500

    upgrade patch

--- Gateway/Request/BaseRequest.php
+++ Gateway/Request/BaseRequest.php
@@ -56,7 +56,7 @@ class BaseRequest implements BuilderInterface
         return [
             'xVersion' => '4.5.8',
             'xSoftwareName' => 'Magento ' . $this->productMetadata->getEdition() . " ". $this->productMetadata->getVersion(),
-            'xSoftwareVersion' => '1.0.9',
+            'xSoftwareVersion' => '1.0.10',
             'xKey' => $this->config->getValue(
                 'cardknox_transaction_key',
                 $order->getStoreId()
