72c68a1fc99c590a3f6b363a77305ebfec0670f7
scali-ck <scali@cardknox.com>
2023-03-27 12:30:32 -0400

    Installed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cardknox/cardknox',
+        'pretty_version' => '1.0.15',
+        'version' => '1.0.15.0',
+        'reference' => NULL,
+        'type' => 'magento2-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cardknox/cardknox' => array(
+            'pretty_version' => '1.0.15',
+            'version' => '1.0.15.0',
+            'reference' => NULL,
+            'type' => 'magento2-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
