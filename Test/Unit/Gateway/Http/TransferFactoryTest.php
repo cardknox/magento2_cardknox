323ea2ac5c5d0a595d743bdac6ff7a04631e6a47
scali-ck <scali@cardknox.com>
2023-03-28 10:23:18 -0400

    test

--- Test/Unit/Gateway/Http/TransferFactoryTest.php
+++ Test/Unit/Gateway/Http/TransferFactoryTest.php
@@ -5,10 +5,10 @@
  */
 namespace Magento\SamplePaymentProvider\Test\Unit\Gateway\Http;
 
-use ..\Gateway\Http\TransferFactory;
+use Gateway\Http\TransferFactory;
 use Magento\Payment\Gateway\Http\TransferBuilder;
 use Magento\Payment\Gateway\Http\TransferInterface;
-use ..\Gateway\Config\Config;
+use Gateway\Config\Config;
 
 class TransferFactoryTest extends \PHPUnit\Framework\TestCase
 {
