428f34877fe941361b4de3b6455e9c26b1809bab
Pritesh Modi <pritesh.lr@gmail.com>
2024-04-19 11:54:20 +0530

    fix sonarlint: Remove duplicate content

--- Model/Config/Backend/Source/ConfigGooglePayButtonType.php
+++ Model/Config/Backend/Source/ConfigGooglePayButtonType.php
@@ -4,6 +4,16 @@ namespace CardknoxDevelopment\Cardknox\Model\Config\Backend\Source;
 
 class ConfigGooglePayButtonType implements \Magento\Framework\Data\OptionSourceInterface
 {
+    public const GPAY_BUTTON_TYPE_PAY = 'pay';
+    public const GPAY_BUTTON_TYPE_BUY = 'buy';
+    public const GPAY_BUTTON_TYPE_PLAIN = 'plain';
+    public const GPAY_BUTTON_TYPE_ORDER = 'order';
+    public const GPAY_BUTTON_TYPE_DONATE = 'donate';
+    public const GPAY_BUTTON_TYPE_CONTINUE = 'continue';
+    public const GPAY_BUTTON_TYPE_CHECKOUT = 'checkout';
+    public const GPAY_BUTTON_TYPE_SUBSCRIBE = 'subscribe';
+    public const GPAY_BUTTON_TYPE_BOOK = 'book';
+
     /**
      * Possible Google Pay button types
      *
@@ -13,40 +23,40 @@ class ConfigGooglePayButtonType implements \Magento\Framework\Data\OptionSourceI
     {
         return [
             [
-                'value' => 'pay',
-                'label' => __('Pay')
+                'value' => self::GPAY_BUTTON_TYPE_PAY,
+                'label' => __('pay')
             ],
             [
-                'value' => 'buy',
-                'label' => __('Buy')
+                'value' => self::GPAY_BUTTON_TYPE_BUY,
+                'label' => __('buy')
             ],
             [
-                'value' => 'plain',
-                'label' => __('Plain')
+                'value' => self::GPAY_BUTTON_TYPE_PLAIN,
+                'label' => __('plain')
             ],
             [
-                'value' => 'order',
-                'label' => __('Order')
+                'value' => self::GPAY_BUTTON_TYPE_ORDER,
+                'label' => __('order')
             ],
             [
-                'value' => 'donate',
-                'label' => __('Donate')
+                'value' => self::GPAY_BUTTON_TYPE_DONATE,
+                'label' => __('donate')
             ],
             [
-                'value' => '',
-                'label' => __('Continue')
+                'value' => self::GPAY_BUTTON_TYPE_CONTINUE,
+                'label' => __('continue')
             ],
             [
-                'value' => 'checkout',
-                'label' => __('Checkout')
+                'value' => self::GPAY_BUTTON_TYPE_CHECKOUT,
+                'label' => __('checkout')
             ],
             [
-                'value' => 'subscribe',
-                'label' => __('Subscribe')
+                'value' => self::GPAY_BUTTON_TYPE_SUBSCRIBE,
+                'label' => __('subscribe')
             ],
             [
-                'value' => 'book',
-                'label' => __('Book')
+                'value' => self::GPAY_BUTTON_TYPE_BOOK,
+                'label' => __('book')
             ],
         ];
     }
