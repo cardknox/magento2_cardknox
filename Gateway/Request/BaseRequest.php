9b2a450677a20aeaeb4e8fefa60dba273d9c442b
Moshe Stein <47797106+mstein-ck@users.noreply.github.com>
2025-11-07 13:35:48 -0500

    version 2.81

--- Gateway/Request/BaseRequest.php
+++ Gateway/Request/BaseRequest.php
@@ -69,7 +69,7 @@ class BaseRequest implements BuilderInterface
         return [
             'xVersion' => '5.0.0',
             'xSoftwareName' => 'Magento ' . $edition . " ". $version,
-            'xSoftwareVersion' => '1.2.78',
+            'xSoftwareVersion' => '1.2.81',
             'xKey' => $this->config->getValue(
                 'cardknox_transaction_key',
                 $order->getStoreId()
