e1940c803a450e26c8c69f8cf0bb4caf483d0ba1
Moshe Stein <47797106+mstein-ck@users.noreply.github.com>
2025-11-07 13:36:41 -0500

    version 2.81 (#94)

--- view/frontend/web/js/view/payment/method-renderer/cardknox-method.js
+++ view/frontend/web/js/view/payment/method-renderer/cardknox-method.js
@@ -259,7 +259,7 @@ define(
                  * [Required]
                  * Set your account data using setAccount(ifieldKey, yourSoftwareName, yourSoftwareVersion).
                  */
-                setAccount(window.checkoutConfig.payment.cardknox.tokenKey, "Magento2", "1.2.78");
+                setAccount(window.checkoutConfig.payment.cardknox.tokenKey, "Magento2", "1.2.81");
 
                 /*
                  * [Optional]
